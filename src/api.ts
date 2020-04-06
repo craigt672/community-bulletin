c125cfb649f27764a008bbfcb6ff8b8b089fcf22
Craig Thomas <craigt672@gmail.com>
2020-04-05 22:02:44 -0400

    create data api layer

--- src/api.ts
+++ src/api.ts
@@ -7,25 +7,108 @@ const API = axios.create({
 });
 
 API.interceptors.response.use(
-  (res) => res.data.data,
+  (res) => res.data,
   (err: any) => Promise.reject(err)
 );
 
-export const getData: any = async () => {
-  return await API.get('/');
+interface IPost {
+  _id: string;
+  title: string;
+  body: string;
+  votes: number;
+  tags: string[];
+  createdAt: number,
+  updatedAt: number
 }
 
-export function useData() {
-  const [data, setData] = useState('');
+export function usePosts() {
+  const [posts, setPosts] = useState<IPost[]>([]);
 
   const setIntialData = () => {
-    (async() => {
-      const initialData = await getData();
-      setData(initialData)
-    })()
+    (async () => {
+      await getPosts();
+    })();
   }
 
-  useEffect(setIntialData, [data]);
+  const getPosts: any = async () => {
+    try {
+      const posts: IPost[] = await API.get('');
+      console.log('posts: ', posts);
+      setPosts(posts);
+    } catch (error) {
+      return error;
+    }
+  }
 
-  return data;
+  const updatePost: any = async (id: string, post: any) => {
+    const unselectedPosts = posts.filter(post => post._id !== id);
+
+    try {
+      const updatedPost: IPost = await API.put(`/${id}`, post);
+      setPosts([...unselectedPosts, updatedPost]);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  const deletePost: any = async (id: string) => {
+    const unselectedPosts = posts.filter(post => post._id !== id);
+
+    try {
+      await API.delete(`/${id}`);
+      setPosts([...unselectedPosts]);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  const createPost: any = async (id: string, post: any) => {
+    try {
+      const createdPost: IPost = await API.post('/', post);
+      setPosts([...post, createdPost]);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  const updateVote: any = (id: string, type: string) => {
+    const unselectedPosts = posts.filter(post => post._id !== id);
+    const foundPost: any = posts.find(post => post._id === id);
+
+    const upvote = async() => {
+      const votes = foundPost.votes + 1;
+      const updatedPost: IPost = await API.put(`/${id}`, { votes });
+      const posts = [...unselectedPosts, updatedPost];
+      setPosts(posts);
+      return posts;
+    }
+
+    const downvote = async() => {
+      const votes = foundPost.votes - 1;
+      const updatedPost: IPost = await API.put(`/${id}`, { votes });
+      const posts = [...unselectedPosts, updatedPost];
+      setPosts(posts);
+      return posts;
+    }
+
+    switch (type) {
+      case 'upvote':
+        return upvote();
+      case 'downvote':
+        return downvote();
+      default:
+        return posts;
+    }
+  }
+
+  useEffect(setIntialData, []);
+
+  return {
+    posts,
+    updatePost,
+    updateVote,
+    createPost,
+    getPosts,
+    deletePost
+  };
 }
